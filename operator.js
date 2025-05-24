692e29923d48f412dc4a79cb8a7ae95b8109e108
Yash Badola <1000014544@dit.edu.in>
2025-05-24 14:13:48 +0530

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,24 @@
+// to check weather the two number are equal or not
+
+
+/*var num1 = Number(prompt("enter the first number:"));
+var num2 = Number(prompt("enter the second number:"))
+num1 === num2? document.writeln(`${num1} is equal to ${num2}`):document.writeln(`${num2} is equal to ${num1}`);
+
+// to check weather the person can vote or not 
+
+let age = Number(prompt("enter the age:"));
+
+age >= 18 ? document.writeln(`${age} you are eligible to vote`):document.writeln(`${age} you are not eligible to vote`);
+
+// to check weather a number is perfect square or not 
+
+let num = Number(prompt("enter the number:"));
+
+Math.sqrt(num) % 1 === 0?document.writeln(`${num} is a perfect square`):document.writeln(`${num} is not a perfect square`);*/
+
+// to check weather the number is even or odd 
+
+let num3 = Number(prompt("enter the number:"));
+
+num3 % 2 == 0?document.writeln(`${num3} is even`):document.writeln(`${num3} is odd`);
