ca8bd54e364486354006306291c7a4b0f3101613
Yash Badola <1000014544@dit.edu.in>
2025-05-29 23:46:48 +0530

    Add files via upload

---
+++ else.js
@@ -0,0 +1,25 @@
+// to check weather the two numbers are equal or not
+
+// to check weather the two person can vote or not
+
+// to check weather a number is perfect square or not 
+
+//Write a program to check if a number is even, else print it's odd.
+
+//Check if a user is an adult, else print “You are a minor.”
+
+//Print "Valid Email" if the email includes "@", else "Invalid Email".
+
+//If temperature is above 30, print “Hot”, else print “Normal”.
+
+//Check if a user is online, else print “User is offline”.
+
+//Check if a light is on, else turn it on.
+
+//If number is positive, print “Positive”, else “Negative”.
+
+//Check if a password length is >= 8, else print “Too short”.
+
+//If marks >= 33, print “Pass”, else “Fail”.
+
+//If age is more than 60, print “Senior Citizen”, else “Not a Senior”.
