722044fb3dab7555dd87e76e880c3b2d46a03993
Yash Badola <1000014544@dit.edu.in>
2025-05-31 17:55:02 +0530

    Add files via upload

---
+++ else-if.js
@@ -0,0 +1,25 @@
+// to check weather the two numbers are equal or not
+
+// to check weather the two person can vote or not
+
+// to check weather a number is perfect square or not 
+
+//Check if a number is positive, negative, or zero.
+
+//Print grade: if marks >= 90 A+, else if >= 75 A, else if >= 60 B, else Fail.
+
+//Determine if a day number (1–7) is Monday to Sunday.
+
+//If age < 13, print “Child”; else if < 20, “Teenager”; else “Adult”.
+
+//Check if the month number is 1 for Jan, 2 for Feb, etc., else print “Invalid”.
+
+//Check a number: < 10, = 10, > 10.
+
+//If input is "admin", print “Welcome Admin”; else if "user", print “Hello User”; else “Unknown”.
+
+//If battery percent > 80, “High”; > 30, “Medium”; else “Low”.
+
+//If traffic light is "red", print “Stop”; "yellow", “Ready”; "green", “Go”; else “Invalid”.
+
+//Check if a number is divisible by 2, else if by 3, else print “Not divisible by 2 or 3”.
