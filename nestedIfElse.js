111f1e78c0366aeac27d4c5fef7093e793c413e7
Yash Badola <1000014544@dit.edu.in>
2025-06-04 12:39:13 +0530

    Add files via upload

---
+++ nestedIfElse.js
@@ -0,0 +1,25 @@
+// to check weather the two numbers are equal or not
+
+// to check weather the two person can vote or not
+
+// to check weather a number is perfect square or not 
+
+//Check if a number is positive. If yes, check if it is even.
+
+//Check if user is logged in. If yes, check if they are admin.
+
+//If marks >= 50, check if marks >= 90 to assign “A+”.
+
+//If weather is “rainy”, check if user has umbrella.
+
+//If age >= 18, check if user has ID card for voting.
+
+//If number is divisible by 5, check if it's also divisible by 10.
+
+//If temperature > 30, check if it’s above 40 (very hot).
+
+//If user has balance > 0, check if they can purchase item (price check).
+
+//If email includes "@", check if it ends with ".com".
+
+//If user is active, check if subscription is valid.
