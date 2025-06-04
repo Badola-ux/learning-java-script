e3196d4f40219ee5a1ae12dffdff0b2e6dbb5582
Yash Badola <1000014544@dit.edu.in>
2025-06-04 12:34:54 +0530

    Add files via upload

--- else-if.js
+++ else-if.js
@@ -1,8 +1,80 @@
-// to check weather the two numbers are equal or not
+// wap to print day name when the user enter day number
+let dayNumber = Number(prompt("Enter the day number (1-7):"));
+if(dayNumber === 1){
+    document.writeln(`${dayNumber} Monday`);
+}
+else if(dayNumber === 2){
+    document.writeln(`${dayNumber} Tuesday`);
+}
+else if(dayNumber === 3){
+    document.writeln(`${dayNumber} Wednesday`);
+}
+else if(dayNumber === 4){
+    document.writeln(`${dayNumber} Thursday`);
+}
+else if(dayNumber === 5){
+    document.writeln(`${dayNumber} Friday`);
+}
+else if(dayNumber === 6){
+    document.writeln(`${dayNumber} Saturday`);
+}
+else if(dayNumber === 7){
+    document.writeln(`${dayNumber} Sunday`);
+}
+else{
+    document.writeln(`${dayNumber} Invalid day number`)
+}
 
-// to check weather the two person can vote or not
+// wap to make a simple calculator
 
-// to check weather a number is perfect square or not 
+let num1  = Number(prompt("Enter the first number:"));
+let operator = prompt("Enter the operator(+,-,*,/):");
+let num2 = Number(prompt("Enter the second number:"));
+if(operator === "+"){
+    document.writeln(`${num1} + ${num2} = ${num1 + num2} add the two number:`);
+}
+else if(operator === "-"){
+    document.writeln(`${num1} - ${num2} = ${num1 - num2} subtract the two number:`);
+}
+else if(operator === "*"){
+    document.writeln(`${num1} * ${num2} = ${num1 * num2} multiply the two number:`);
+}
+else if(operator === "/"){
+    document.writeln(`${num1} / ${num2} = ${num1 / num2} divide the two number:`);
+}
+else{
+    document.writeln(`${"Invalid operator"}`);
+}
+
+// wap to check weather an entered character is lower case vowel, lower case consonant, upper case consonant, digit, space or any special character
+
+var ch = prompt("Enter the single character:");
+if(ch.length!= 1){
+    document.writeln(`"${ch}" is invalid. please enter the single character:`);
+}
+else if(ch>='a'&&ch<='z'){
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+        document.writeln(`${ch} is a lower case Vowel`);
+    }else{
+        document.writeln(`${ch} is a lower case consonant`);
+        }
+}
+else if(ch>='A'&&ch<='Z'){
+    if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+        document.writeln(`${ch} is a upper case Vowel`);
+    }else{
+        document.writeln(`${ch} is a upper case consonant`);
+        }
+}
+else if(ch>='0'&&ch<='9'){
+    document.writeln(`${ch} is a digit`);
+}
+else if(ch==' '){
+    document.writeln(`${ch} is a space`);
+}
+else{
+    document.writeln(`${ch} is a character`);
+}
 
 //Check if a number is positive, negative, or zero.
 
