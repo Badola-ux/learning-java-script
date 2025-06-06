2d026209baee16eeb183bb6b61802a49772ed0f4
Yash Badola <1000014544@dit.edu.in>
2025-06-06 11:12:33 +0530

    Add files via upload

---
+++ newest.js
@@ -0,0 +1,9 @@
+console.log("hello!");
+alert("hey this is zombie");
+
+//let heading = document.getElementById("heading");
+//console.log(heading);
+
+let headings = document.getElementsByTagName("heading-class");
+console.dir(headings);
+console.log(headings);
