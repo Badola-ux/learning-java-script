51f4162999d4ab3ac0733fe13b660314517e127e
Yash Badola <1000014544@dit.edu.in>
2025-06-16 14:54:30 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,74 @@
+/*
+syntax
+function functionName(parameters)
+------------
+------------
+number of statements
+------------
+------------
+return value
+}
+functionName(arguments)
+*/
+
+//sample case
+
+function test(){
+    console.log("In test function");
+}
+console.log("In main outer scope");
+test()
+test()
+test()
+test()
+test()
+console.log("Back to main outer scope");
+
+// function call within function 
+
+function fun1(){
+    console.log("In fun1 function");
+    fun2()
+    console.log("Back to fun1 function");
+}
+function fun2(){
+    console.log("In fun2 function");
+    fun3()
+    console.log("Back to fun2 function");
+}
+function fun3(){
+    console.log("In fun3 function");
+}
+console.log("In main global scope");
+fun1()
+console.log("Back to main global scope");
+
+// function without return value without parameter
+
+function add(){
+    let a = 10;
+    let b = 20;
+    let sum = a + b;
+    console.log(`sum = ${sum}`);
+}
+function test(){
+    add()
+}
+test()
+
+// function with return value without parameter
+
+function add(){
+    let a = 6;
+    let b = 7;
+    let sum = a + b;
+    return sum;
+}
+function test(){
+    let s = add();
+    console.log(`sum = ${s}`); // multiple time will be used for single time use 'add' in place of 's'
+}
+test()
+
+// function without return value with parameter
+// function with return with parameter 
