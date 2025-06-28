b06910736d4a597b811f3116012136f10ae55b4c
Yash Badola <1000014544@dit.edu.in>
2025-06-28 12:20:40 +0530

    Add files via upload

--- nestedLoops.js
+++ nestedLoops.js
@@ -1,5 +1,31 @@
 // wap small eg of nested loops
+
+for(let i=1; i<=5; i++){
+    let row = "";
+    for(let j = 1; j <= i; j++){
+        row = row + "* "
+    }
+    console.log(row);
+}
 // wap to print all perfect number (5, 28, 496, 8128) in a range
+
+let num2 = Number(prompt("Enter the number:"));
+let start = 1;
+let end = 10000000000;
+while(num2<=end){
+    let sum = 0;
+    let i = 1;
+    while(i<=num2/2){
+        if(num2%i==0){
+            sum = sum + i;
+        }
+        i++;
+    }
+    if(sum===num2){
+        document.writeln(`${num2} is a perfect number`);
+    }
+    num2++;
+}
 // wap to print and count all palindrome number in a range 
 // wap to print and count all armstrong numbers in a range (153, 370, 371, 407)
 // wap to print and count all magical number(81, 1458, 1729)
@@ -27,3 +53,4 @@ if (isMagicNumber(num)) {
 }
 
 // wap to print and count all prime number in a range 
+
