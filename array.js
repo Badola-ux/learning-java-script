ac1e36029f53dde504256690a0ada6d151f5be41
Yash Badola <1000014544@dit.edu.in>
2025-07-29 23:09:23 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,23 @@
+// wap to calculate the sum of even and odd elements of an array 
+
+let arr = [4,5,6,10,20,30,40,50,23,34,77,100];
+let evenSum = 0;
+let oddSum = 0;
+for(let i = 0; i < arr.length; i++){
+    if(arr[i]%2===0){
+        evenSum = evenSum + arr[i];
+    }else{
+        oddSum = oddSum + arr[i];
+    }
+}
+console.log(`${evenSum} sum of even elements`);
+console.log(`${oddSum} sum of odd elements`);
+
+// wap to count and print prime number from an array 
+
+let arr1 = [1,2,3,4,5,6,7,7,8,9];
+let 
+// wap to find the smallest element from an array without sorting them 
+// wap to find the greatest element from an array without sorting them 
+// wap to find first smallest, second smallest and the third smallest element from an array without sorting them 
+// wap to find nth smallest element from an array without sorting them 
