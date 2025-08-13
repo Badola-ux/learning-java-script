56f84dd62d5be9dbe81300dc951ef661b8c6e76a
Yash Badola <1000014544@dit.edu.in>
2025-08-13 23:36:48 +0530

    Add files via upload

--- arrayFlat.js
+++ arrayFlat.js
@@ -7,3 +7,13 @@ var b = [[[10,20,30],[40,50,60],70,[80,90,100]]];
 console.log(b);
 console.log(b.flat(2));
 console.log(b.flat(2).map(x => x*5));
+
+var c = [[[[10,20,30],[40,50,60],70,[80,90,100]]]];
+console.log(c);
+console.log(c.flat(3));
+console.log(c.flat(3).map(x => x*5));
+
+var d = [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[10,20,30],[40,50,60],70,[80,90,100]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]];
+console.log(d);
+console.log(d.flat(Infinity));
+console.log(d.flat(Infinity).map(x => x*5));
