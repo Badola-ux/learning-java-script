5adc6357613c49c9caff04b1e9be38415ebda1ef
Yash Badola <1000014544@dit.edu.in>
2025-10-25 11:33:54 +0530

    Add files via upload

---
+++ synchronous.js
@@ -0,0 +1,22 @@
+function fun1(){
+    console.log("hello world1");
+}
+function fun2(){
+    console.log("hello world2");
+}
+function fun3(){
+    console.log("hello world3");
+}
+function fun4(){
+    console.log("hello world4");
+}
+function fun5(){
+    console.log("hello world5");
+}
+fun1()
+fun2()
+fun3()
+fun4()
+fun5()
+
+// one task has to wait to complete the previous task that is line by line execution 
