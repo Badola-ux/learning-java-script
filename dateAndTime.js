7c666689dc6d04a611dea7395ecc8c4559a03308
Yash Badola <1000014544@dit.edu.in>
2025-10-31 11:45:11 +0530

    Add files via upload

--- dateAndTime.js
+++ dateAndTime.js
@@ -1,23 +1,33 @@
 // date and time 
 // 1 style - new date(): return current date and time
 
-var a = newDate();
+var a = new Date();
 console.log(a);
 console.log(a.toString());
 console.log(a.toDateString());
 console.log(a.toTimeString());
-console.log(a.toLocalString());
-console.log(toLocaleDateString());
+console.log(a.toLocaleString());
+console.log(a.toLocaleDateString());
 console.log(a.toLocaleTimeString());
 
 // 2nd type - new date layer, month, date, hour, minute, second, millisecond
 // we have to specify at least two items in date 
 // if we pass a single argument in date then it treat as milliseconds 
 
-var b = newDate(2025, 7, 12, 15, 30, 30, 2000);
-var c = newDate(2025, 7, 12, 15, 30, 30);
-var d = newDate(2025, 7, 12, 15, 30);
+var b = new Date(2025, 7, 12, 15, 30, 30, 2000);
+var c = new Date(2025, 7, 12, 15, 30, 30);
+var d = new Date(2025, 7, 12, 15, 30);
 
-console.log(b.toLocalString());
-console.log(c.toLocalString());
-console.log(d.toLocalString());
+console.log(b.toLocaleString());
+console.log(c.toLocaleString());
+console.log(d.toLocaleString());
+
+// 3rd style:: new Date(millisecond)
+
+var e = new Date(123456789000000);
+console.log(e.toLocaleString());
+
+// 4th style : new Date(datesString)
+
+var f = new Date("2025/08/12 15:30:33");
+console.log(f.toLocaleString());
